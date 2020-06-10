8533e760bc722a46699c085ddc0371d2a559ba98
Myrian Katto <myriankatto@gmail.com>
2020-06-10 14:41:21 -0300

    signup and login routes

--- functions/index.js
+++ functions/index.js
@@ -54,6 +54,20 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === '') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 //Signup Route
 
 app.post('/signup', (req, res) => {
@@ -64,8 +78,22 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle,
   };
 
-  //TODO: validate data
+  let errors = {};
 
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Password must match';
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
+
+  //TODO: validate data
+  let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then((doc) => {
@@ -76,10 +104,22 @@ app.post('/signup', (req, res) => {
       }
     })
     .then((data) => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
 
-    .then((token) => {
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+
+    .then(() => {
       return res.status(201).json({ token });
     })
 
@@ -93,4 +133,35 @@ app.post('/signup', (req, res) => {
     });
 });
 
+//Login Route
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = 'Must not be empty';
+  if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/wrong-password') {
+        return res.status(403).json({ general: 'Wrong credentials, please try again' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
 exports.api = functions.https.onRequest(app);
