fb487c773e1cbbc22a3cdd89a0c0de0ccb1da537
Myrian Katto <myriankatto@gmail.com>
2020-06-11 16:05:04 -0300

    commit

---
+++ socialape-functions/functions/utilities/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyATgYz0iR9t2wdCWv7K1s2hD-Ttt17fiMw',
+  authDomain: 'socialape-7cf41.firebaseapp.com',
+  databaseURL: 'https://socialape-7cf41.firebaseio.com',
+  projectId: 'socialape-7cf41',
+  storageBucket: 'socialape-7cf41.appspot.com',
+  messagingSenderId: '964318894904',
+};
