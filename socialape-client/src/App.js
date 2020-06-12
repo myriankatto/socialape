9cd453f96119f09d46781610fe5718aae12c8c06
Myrian Katto <myriankatto@gmail.com>
2020-06-12 12:35:03 -0300

    react login page

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 //Components
 import NavBar from './components/NavBar';
@@ -12,19 +14,42 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#8561c5',
+      main: '#673ab7',
+      dark: '#482880',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#637bfe',
+      main: '#3d5afe',
+      dark: '#2a3eb1',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-      <NavBar />
-       <div className="container"> 
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-        </Switch></div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- socialape-client/src/components/NavBar.js
+++ socialape-client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
 
 //Material UI Stuff
 import AppBar from '@material-ui/core/AppBar';
